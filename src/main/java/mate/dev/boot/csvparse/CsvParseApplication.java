9c4c5afff217af36b45fc12b51bb783e85d2ec4f
Dmitrii <zinchukdimaa@gmail.com>
2020-03-02 09:32:27 +0200

    init commit

---
+++ src/main/java/mate/dev/boot/csvparse/CsvParseApplication.java
@@ -0,0 +1,13 @@
+package mate.dev.boot.csvparse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvParseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvParseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/dev/boot/csvparse/CsvParseApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.dev.boot.csvparse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvParseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
